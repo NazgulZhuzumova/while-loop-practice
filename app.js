0c6448a5acd31793442f9323b7c8b9e3747a4071
NazgulZhuzumova <nazgul.zhuzumova@yahoo.co.uk>
2022-07-21 16:13:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/*const fruits = ['banana', 'berries', 'apple', 'oranges'];
+let i = 0;
+let text = "";
+while(fruits[i]) {
+    text += fruits[i];
+    i++;
+}
+console.log(fruits) */
+
+// while loop  simple practice
+
+/*let i = 0;
+while (i < 100) {
+    console.log(i)
+    i = i + 2;
+}*/
+
+/* for (i = 0; i < 10; i++){
+    console.log(i);
+    if (i == 5) {
+        break;
+    }
+} */
+for (i = 0; i < 10; i++) {
+    if (i == 5) {
+        continue;
+    } 
+    console.log(i);
+}
